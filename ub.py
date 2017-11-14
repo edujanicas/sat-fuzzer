5ea319437e149c307cc0a8020b70c71062dc3be3
Requiel20 <luca.battistelli@studenti.unicam.it>
2017-11-14 18:47:00 +0000

    ub catches undefined behaviour but no stdout, inputs random valid cnfs

--- ub.py
+++ ub.py
@@ -1,5 +1,6 @@
-import os.path
 import random
+import re
+import string
 import subprocess
 import tempfile
 
@@ -7,6 +8,15 @@ import util
 
 SAVED_INPUTS_PATH = "fuzzed-tests/"
 saved_inputs_id = 0
+
+REGEXES = {
+    "INTMIN_NEGATED_REGEX": re.compile('^runtime.+negation'),
+    "NULLPOINTER_REGEX": re.compile('^runtime.+null pointer'),
+    "SHIFT_ERROR_REGEX": re.compile('^runtime.+shift'),
+    "USE_AFTER_FREE_REGEX": re.compile('^==.*AddressSanitizer: heap-use-after-free'),
+    "HEAP_BUFFER_OVERFLOW_REGEX": re.compile('^==.*AddressSanitizer: heap-buffer-overflow'),
+    "STACK_BUFFER_OVERFLOW_REGEX": re.compile('^==.*AddressSanitizer: stack-buffer-overflow'),
+    "SIGNED_INTEGER_OVERFLOW_REGEX": re.compile('^runtime.+signed integer')}
 
 
 def do_ub(sut_path, inputs_path, seed):
@@ -18,25 +28,47 @@ def do_ub(sut_path, inputs_path, seed):
     while True:
         input_file = create_input()
 
-        # TODO: timeout
-        ub = run_sut(input_file, sut_path)
+        sut_output = run_sut(input_file, sut_path)
 
-        if ub is not None:
-            save_input(input_file)
+        if sut_output is not None:
+            ubs = check_ub(sut_output)
+
+            if ubs > 0:
+                save_input(input_file)
+
+        input_file.close()
 
 
 def create_input():
-    cnf = "p cnf 0 0"
+    cnf = "p cnf 10 100\n"
+
+    for i in range(0, 100):
+        for j in range(0, 5):
+            cnf += random.choice(string.digits) + " "
+        cnf += "0\n"
+
     input_file = tempfile.NamedTemporaryFile(mode='w')
     input_file.write(cnf)
+    input_file.flush()
     return input_file
 
 
 def run_sut(input_file, sut_path):
-    result = subprocess.run([os.path.join(sut_path, "runsat.sh"), input_file.name], stderr=subprocess.PIPE)
-    run_output = result.stderr.decode('ascii').split('\n')[:-1]
-    # TODO check for undefined behaviour
-    return True
+    result = subprocess.Popen(["./runsat.sh", input_file.name], stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+                              cwd=sut_path)
+
+    try:
+        sut_output, sut_error = result.communicate(timeout=5)
+        for line in sut_output.decode('ascii'):
+            print(line)
+        return sut_error
+
+    except subprocess.TimeoutExpired:
+        result.kill()
+        result.communicate()
+        print("TIMED OUT")
+
+    return None
 
 
 def save_input(input_file):
@@ -46,3 +78,14 @@ def save_input(input_file):
     util.save_text(SAVED_INPUTS_PATH + "interesting_input{}.txt".format(saved_inputs_id), input_file.name)
 
     saved_inputs_id = (saved_inputs_id + 1) % 20
+
+
+def check_ub(sut_error):
+    ubs = 0
+    for line in sut_error.decode('ascii '):
+        for key, value in REGEXES.items():
+            if value.match(line):
+                print(line)
+                ubs += 1
+
+    return ubs
