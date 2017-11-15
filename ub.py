6359caceb8a26001e127de83a932552cfbb57087
Requiel20 <luca.battistelli@studenti.unicam.it>
2017-11-15 18:45:19 +0000

    ub working

--- ub.py
+++ ub.py
@@ -5,20 +5,25 @@ import string
 import subprocess
 import tempfile
 
-import util
+from shutil import copy2
 
 SAVED_INPUTS_PATH = "fuzzed-tests/"
 saved_inputs_id = 0
 
 REGEXES = {
-    "INTMIN_NEGATED_REGEX": re.compile('^runtime.+negation'),
-    "NULLPOINTER_REGEX": re.compile('^runtime.+null pointer'),
-    "SHIFT_ERROR_REGEX": re.compile('^runtime.+shift'),
+    "INTMIN_NEGATED_REGEX": re.compile('^.*runtime.+negation'),
+    "NULLPOINTER_REGEX": re.compile('^.*runtime.+null pointer'),
+    "SHIFT_ERROR_REGEX": re.compile('^.*runtime.+shift'),
     "USE_AFTER_FREE_REGEX": re.compile('^==.*AddressSanitizer: heap-use-after-free'),
     "HEAP_BUFFER_OVERFLOW_REGEX": re.compile('^==.*AddressSanitizer: heap-buffer-overflow'),
     "STACK_BUFFER_OVERFLOW_REGEX": re.compile('^==.*AddressSanitizer: stack-buffer-overflow'),
-    "SIGNED_INTEGER_OVERFLOW_REGEX": re.compile('^runtime.+signed integer')
+    "SIGNED_INTEGER_OVERFLOW_REGEX": re.compile('^.*runtime.+signed integer')
 }
+
+INTERSTING_CNFS = ["p cnf 0 0",
+                   "p cnf 10 20\n!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+                   "p cnf 10 20\n\n\n0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ 	\n0\n00123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ 	\n",
+                   "p cnf 10 20\n\n\n01234567890123456789"]
 
 DIGITS_NO_ZERO = string.digits.replace("0", "")
 
@@ -30,14 +35,21 @@ def do_ub(sut_path, inputs_path, seed):
         random.seed()
 
     j = 0
+    i = 0
 
-    for i in range(0, 1):
-        if j == 0:
-            print("Sending input")
-            input_file = create_input()
-        elif j == 1:
-            print("Sending garbage")
-            input_file = create_garbage()
+    while True:
+        if i <= len(INTERSTING_CNFS) - 1:
+            cnf = INTERSTING_CNFS[i]
+            print("Sending " + cnf)
+        else:
+            if j == 0:
+                print("Sending input")
+                cnf = create_input()
+            else:
+                print("Sending garbage")
+                cnf = create_garbage()
+
+        input_file = make_cnf_file(cnf)
 
         sut_output = run_sut(input_file, sut_path)
 
@@ -51,6 +63,7 @@ def do_ub(sut_path, inputs_path, seed):
 
         j += 1
         j = i % 2
+        i += 1
 
 
 def create_input():
@@ -58,15 +71,16 @@ def create_input():
     NUMBER_OF_LITERALS = 999
     formulas_width = 10
 
-    cnf = "p cnf " + str(NUMBER_OF_LITERALS) + " " + str(number_of_formulas) + "\n"
-
+    cnf = "p cnf " + str(NUMBER_OF_LITERALS) + " " + str(number_of_formulas) + "\n999 0\n"
     for i in range(0, number_of_formulas):
         for j in range(0, formulas_width):
             cnf += ["", "-"][int(random.random() * 1)]
-            cnf += DIGITS_NO_ZERO[int(random.random() * 9)] + " "
+            for k in range(0, 3):
+                cnf += DIGITS_NO_ZERO[int(random.random() * 9)]
+            cnf += " "
         cnf += "0\n"
 
-    return make_cnf_file(cnf)
+    return cnf
 
 
 def create_garbage():
@@ -90,7 +104,7 @@ def create_garbage():
 
         choice = random.randint(0, 5)
 
-    return make_cnf_file(cnf)
+    return cnf
 
 
 def make_cnf_file(cnf):
@@ -104,34 +118,39 @@ def run_sut(input_file, sut_path):
     result = subprocess.Popen(["./runsat.sh", input_file.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                               cwd=sut_path)
 
+    sut_error = None
+
     try:
-        sut_output, sut_error = result.communicate()
+        sut_output, sut_error = result.communicate(timeout=20)
+
+
         sut_output_printable = sut_output.decode('ascii').split('\n')
+
         for line in sut_output_printable:
             print(line)
-        return sut_error
 
     except subprocess.TimeoutExpired:
         result.kill()
-        result.communicate()
-        print("TIMED OUT")
 
-    return None
+    return sut_error
 
 
-def save_input(sut_path, input_file):
+def save_input(sut_path, temp_file):
     global saved_inputs_id
 
-    # TODO decide what to do after 20 inputs have been found
-    util.save_text(os.path.join([sut_path, SAVED_INPUTS_PATH, "interesting_input{}.txt".format(saved_inputs_id)]),
-                   input_file.name)
+    os.makedirs(os.path.join(sut_path, SAVED_INPUTS_PATH), exist_ok=True)
+
+    output_file = open(os.path.join(sut_path, SAVED_INPUTS_PATH, "interesting_input{}.txt".format(saved_inputs_id)),
+                       'w+')
+
+    copy2(temp_file.name, output_file.name)
 
     saved_inputs_id = (saved_inputs_id + 1) % 20
 
 
 def check_ub(sut_error):
     ubs = 0
-    for line in sut_error.decode('ascii '):
+    for line in sut_error.decode('ascii ').split('\n'):
         for key, value in REGEXES.items():
             if value.match(line):
                 print(line)
--- util.py
+++ util.py
@@ -1,3 +1,8 @@
+import os
+
+
 def save_text(text_to_save, path):
-    with open(path) as f:
+    with open(path, 'w') as f:
+        os.makedirs(path)
         f.write(text_to_save)
+        f.flush()
